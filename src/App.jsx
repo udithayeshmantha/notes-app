30acd1792f3e94f4b4cd0bb84cf7a048eb351039
Udith Ayeshmantha <85647550+udithayeshmantha@users.noreply.github.com>
2025-02-07 00:36:22 +0530

    add routing and authentication components; integrate Navbar and PasswordInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import Home from './pages/Home/Home'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+
+const RoutesComponent = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  )
+}
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <RoutesComponent />
     </div>
   )
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";1
+import { Link } from "react-router-dom";
+import PasswordInput from "../../components/passwordinput";
 
 const Login = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 p-10 bg-white shadow-lg rounded-lg">
+        <form onSubmit={() => {}}>
+          <h4 className="text-2xl mb-7">Login</h4>
+          <input type="text" placeholder="Email" className="input-box" />
+          <PasswordInput />
+          <button type="submit" className="btn-primary">
+            Login
+          </button>
+          <p className="text-sm text-center mt-4">
+            Not registered yet?{" "}
+            <Link to="/signup" className="font">
+              Create an Account
+            </Link>
+          </p>
+        </form>
+      </div>
+      </div>
+    </>
   )
-}
+};
 
-export default Login
+export default Login;
