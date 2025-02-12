b10a88a75a8f78eb8d37fdbaf1fbd1ee2240cfcd
Udith Ayeshmantha <85647550+udithayeshmantha@users.noreply.github.com>
2025-02-13 00:54:40 +0530

    add NoteCard component for displaying notes; update Profileinfo styling and add icon button styles

---
+++ src/components/Cards/NoteCard.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { MdCreate, MdDelete, MdOutlinePushPin } from "react-icons/md";
+
+const NoteCard = ({
+  title,
+  date,
+  content,
+  tags,
+  isPinned,
+  onEdit,
+  onDelete,
+  onPinNote,
+}) => {
+  return (
+    <div className="border-1 border-gray-200 rounded-md p-4 bg-white hover:shadow-lg transition-all ease-in-out">
+      <div className="flex justify-between items-center">
+        <div>
+          <h6 className="text-sm font-medium">{title}</h6>
+          <span className="text-xs text-slate-500">{date}</span>
+        </div>
+        <MdOutlinePushPin
+          className={`icon-btn ${
+            isPinned ? "text-blue-500" : "text-slate-300"
+          }`}
+          onClick={onPinNote}
+        />
+      </div>
+      <p className="text-xs text-slate-600 mt-2">{content?.slice(0, 60)}</p>
+      <div className="flex justify-between items-center mt-2">
+        <div className="text-xs text-slate-500">{tags}</div>
+        <div className="flex items-center gap-2">
+          <MdCreate
+            className="icon-btn hover:text-green-600"
+            onClick={onEdit}
+          />
+          <MdDelete
+            className="icon-btn hover:text-red-500"
+            onClick={onDelete}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoteCard;
